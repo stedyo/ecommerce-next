871f31b581a8d6c43aca05b06a45430f60474cdb
stephanyzd <stephanydionysio@gmail.com>
2021-09-06 18:23:30 -0300

    google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,7 +19,20 @@ export default class CustomDocument extends Document {
 		}
 		return (
 			<Html dir={getDirection(locale)}>
-				<Head />
+				<Head>
+				{/*<!-- Global site tag (gtag.js) - Google Analytics -->*/}
+					<script async src="https://www.googletagmanager.com/gtag/js?id=G-YMWRY1711T" />
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', '[Tracking ID]', { page_path: window.location.pathname });
+							`,
+						}}
+					/>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
